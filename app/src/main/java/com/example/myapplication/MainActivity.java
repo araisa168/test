0519e46fee38c004caf0f6d41e00c2660f3fcd88
araisa00 <ar.sa.kanoesaru168@nifty.com>
2021-05-16 19:45:38 +0900

    orange修正19:45

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,6 +20,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        String s="";
+        String s="1";
     }
 }
